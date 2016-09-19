1a151c236da01548fd50e5de7b6a8c91c9762753
snoupyrelete <drob8896@740s-pl-112.local>
2016-09-19 09:54:52 -0600

    Setup for turn in.

---
+++ Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let viewOne = UIView(frame: CGRectMake(0, 0, 200, 200))
+viewOne.backgroundColor = UIColor(red: 200, green: 0, blue: 0, alpha: 1)
+viewOne.layer.cornerRadius = 100
